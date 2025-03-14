a3e8b333841a644ac91aece4a64f63c2059fa1cb
Vitor Amorim <vi08tor@gmail.com>
2025-03-13 22:38:07 -0300

    Update exercicio17.py

--- algoritmos/aula1/exercicio17.py
+++ algoritmos/aula1/exercicio17.py
@@ -1,6 +1,5 @@
 # A locadora de carros precisa da sua ajuda para cobrar seus serviços. Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço total a pagar, sabendo que o carro custa R$90,00 por dia e R$0,20 por Km rodado.
 
-
 km = float(input("Digite a quantidade de Km percorridos:"))
 dias = int(input("Digite a quantidade de dias alugados:"))
 custoDiaria = dias * 90.00
