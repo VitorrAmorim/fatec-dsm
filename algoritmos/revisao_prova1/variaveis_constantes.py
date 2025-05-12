6f69271179ed28ac9459630c930cde0cbb19e80d
VitorrAmorim <vi08tor@gmail.com>
2025-05-12 17:09:36 -0300

    iniciando revisao

---
+++ algoritmos/revisao_prova1/variaveis_constantes.py
@@ -0,0 +1,14 @@
+print('Olá Mundo!') #Operação de impressão
+
+print('Bom dia, Vitor.')
+
+nome = 'Vitor' #O identificador nome servirá como variável para armazer a String com valor 'Vitor'
+print('Bom dia ' + nome) #Concatenando (juntando) a String 'Bom dia' com o valor da variável Nome
+
+nome = 'Gabriel' #O valor da String será atualizado para 'Gabriel'
+print('Bom dia ' + nome)
+
+nome = 30 #Particularidade do Python: o mesmo identificador pode armazenar valores de tipos diferentes
+print(nome)
+
+'''1lab el = 10 #Não é permitido iniciar um identificador com número'''
