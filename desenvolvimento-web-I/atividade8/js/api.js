a833766f86fb6bc3f23710792bf0b21b6afc073d
VitorrAmorim <vi08tor@gmail.com>
2025-05-30 09:16:08 -0300

    trocando API

--- desenvolvimento-web-I/atividade8/js/api.js
+++ desenvolvimento-web-I/atividade8/js/api.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
     $('#btnBuscarIP').click(function() {
-        // Usando a API ip-api.com (versão gratuita)
-        $.getJSON('http://ip-api.com/json/?fields=status,message,country,regionName,city,isp,query', function(data) {
-            if (data.status === 'success') {
-                $('#txtip').text('IP: ' + data.query);
-                $('#txtcidade').text('Cidade: ' + data.city);
-                $('#txtregiao').text('Região: ' + data.regionName);
-                $('#txtpais').text('País: ' + data.country);
-                $('#txtprovedor').text('Provedor: ' + data.isp);
+        // Usando a API RandomUser.me
+        $.getJSON('https://randomuser.me/api/', function(data) {
+            if (data.results && data.results.length > 0) {
+                const user = data.results[0];
+                $('#txtip').text('Nome: ' + user.name.first + ' ' + user.name.last);
+                $('#txtcidade').text('Cidade: ' + user.location.city);
+                $('#txtregiao').text('Estado: ' + user.location.state);
+                $('#txtpais').text('País: ' + user.location.country);
+                $('#txtprovedor').text('E-mail: ' + user.email);
             } else {
-                alert('Erro ao buscar informações do IP: ' + data.message);
+                alert('Erro ao buscar informações do usuário: dados não encontrados');
             }
         }).fail(function() {
-            alert('Erro ao conectar com o serviço de IP. Tente novamente mais tarde.');
+            alert('Erro ao conectar com o serviço de usuários. Tente novamente mais tarde.');
         });
     });
 });
